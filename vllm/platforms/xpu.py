35f7b57d4a4df156735b7d0f6498fefe7136b5a9
Wu, Xiaochang <xiaochang.wu@intel.com>
2026-07-11 04:34:01 +0000

    revert to torch.ops._C_cache_ops.getMemoryInfo
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>

--- vllm/platforms/xpu.py
+++ vllm/platforms/xpu.py
@@ -92,7 +92,7 @@ def get_mem_info_wrapper(
         )
 
     # Call the underlying C++ implementation
-    free, total = torch.xpu.mem_get_info(device)
+    free, total = torch.ops._C_cache_ops.getMemoryInfo(device)
 
     return free, total
 
