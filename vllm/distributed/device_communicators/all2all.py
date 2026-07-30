18095f3b291c3ee56e49967351351cb7d5fb0acd
Xinyu Chen <xinyu1.chen@intel.com>
2026-07-30 05:30:19 +0000

    adapt new deepsymm API for combine_reduce_type
    Signed-off-by: Xinyu Chen <xinyu1.chen@intel.com>
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>

--- vllm/distributed/device_communicators/all2all.py
+++ vllm/distributed/device_communicators/all2all.py
@@ -1115,7 +1115,7 @@ class DeepSymmAll2AllManager(All2AllManagerBase):
         self,
         hidden_size: int,
         num_topk: int,
-        hidden_dtype: torch.dtype = torch.bfloat16,
+        combine_reduce_type: torch.dtype = torch.bfloat16,
         dispatch_scale_dtype: torch.dtype = torch.float32,
         dispatch_group_size: int | None = None,
     ):
@@ -1127,7 +1127,7 @@ class DeepSymmAll2AllManager(All2AllManagerBase):
                 num_max_tokens_per_rank=self._max_tokens_per_rank,
                 hidden=hidden_size,
                 num_topk=num_topk,
-                hidden_dtype=hidden_dtype,
+                combine_reduce_type=combine_reduce_type,
                 dispatch_scale_dtype=dispatch_scale_dtype,
                 dispatch_group_size=dispatch_group_size,
             )
